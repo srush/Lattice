6c77c695dc37513278781ac11950020f1d17c294
srush@mit.edu <srush@srush-laptop.(none)>
2010-11-15 09:07:41 -0500

    small lattice

--- util/hypergraph_to_lattice.py
+++ util/hypergraph_to_lattice.py
@@ -17,6 +17,7 @@ class Graph(object):
 
     self.check = set()
     self.lattice = Lattice()
+    self.total_edges = 0
 
   def set_start(self, node):
     self.lattice.start = node.id
@@ -31,7 +32,7 @@ class Graph(object):
     node.id = self.id
     n = self.lattice.node.add()
     n.id = self.id
-    print node
+    #print node
     s = str(node)
     n.label = unicode(s, errors="ignore")
     node.proto = n
@@ -74,11 +75,12 @@ class LatNode(object):
     self.lex = None
     self.graph = graph
     
-  def add_edge(self, to_node):
+  def add_edge(self, to_node, label):
     self.edges.add(to_node)
     to_node.back_edges.add(self)
     edge = self.proto.edge.add()
     edge.to_id = to_node.id
+    return edge 
 
   def label(self):
     return str(self)
@@ -88,8 +90,8 @@ class NonTermNode(LatNode):
     self.forest_node = forest_node
     self.dir = dir
     LatNode.__init__(self, graph)
-    self.proto.Extensions[original_node] = -1
-    self.proto.Extensions[ignore_node] = True
+    #self.proto.Extensions[original_node] = -1
+    #self.proto.Extensions[ignore_node] = True
 
   def __str__(self):
     return "%s %s %s"%(str(self.forest_node.label), self.dir, self.id)
@@ -102,12 +104,12 @@ class LexNode(LatNode):
     self.lex = lex
     LatNode.__init__(self, graph)
     
-    print self.proto._known_extensions
-    self.proto.Extensions[lattice.is_word] = True
-    self.proto.Extensions[lattice.word] = lex.decode("utf-8")
-    print lex, self.id
+    #print self.proto._known_extensions
+    #self.proto.Extensions[lattice.is_word] = True
+    #self.proto.Extensions[lattice.word] = lex.decode("utf-8")
+    #print lex, self.id
     self.graph.register_edge_map(self, edge_id)
-    self.proto.Extensions[original_node] = edge_id
+    #self.proto.Extensions[original_node] = edge_id
 
   def __str__(self):
     return "%s %s"%(self.lex, self.id)
@@ -129,7 +131,7 @@ class InternalNode(LatNode):
       self.pos = pos
     LatNode.__init__(self, graph)
     self.graph.register_edge_map(self, edge_id)
-    self.proto.Extensions[original_node] = edge_id
+    #self.proto.Extensions[original_node] = edge_id
 
   def __str__(self):
     #rhs = self.rule.rhs[:]
@@ -154,33 +156,60 @@ class NodeExtractor(object):
   "Class for creating the FSA that represents a translation forest (forest lattice) "
 
   def __init__(self):
-    pass
+    self.internal_nodes = 0
+    self.word_nodes = 0
+    self.edge_id = 0
+    self.original_id = 0
+    self.total_edges = 0
  
   def extract(self, forest):
     self.memo = {}
     self.graph = Graph()
     self.forest = forest
     first_state = LexNode(self.graph, "<s>", -1)
-    second_state = LexNode(self.graph, "<s>", -1)
-    first_state.add_edge(second_state)
-    
+    first_state.proto.Extensions[has_phrases] = True
+    proto_plet = first_state.proto.Extensions[phraselets].phraselet.add()
+    proto_plet.phraselet_hypergraph_edge = -1
+    for w in ['<s>', '<s>']:  
+      subword = proto_plet.word.add()
+      subword.word = w
+      subword.subword_original_id = self.original_id
+      #print self.original_id, subword.word
+      self.original_id += 1
+
     (first, last) = self.extract_fsa(self.forest.node[forest.root])
     
-    second_state.add_edge(first)
+    first_state.add_edge(first, "")
 
-    next_to_last_state = LexNode(self.graph, "</s>", -1)
     last_state = LexNode(self.graph, "</s>", -1)
+    last_state.proto.Extensions[has_phrases] = True
+    last.add_edge(last_state, "")
     
-    last.add_edge(next_to_last_state)
-    next_to_last_state.add_edge(last_state)
+    
+    proto_plet = last_state.proto.Extensions[phraselets].phraselet.add()
+    proto_plet.phraselet_hypergraph_edge = -1
+    for w in ['</s>', '</s>']:  
+      subword = proto_plet.word.add()
+      subword.word = w
+      subword.subword_original_id = self.original_id
+      #print self.original_id, subword.word
+      self.original_id += 1
 
     self.graph.set_start(first_state)
     self.graph.set_final(last_state)
+    print "Internal Nodes", self.internal_nodes
+    print "Word Nodes", self.word_nodes
+    print "Total Nodes", self.graph.size()
+    
+    print self.total_edges
+    self.graph.lattice.Extensions[num_hypergraph_edges] = self.total_edges
+    self.graph.lattice.Extensions[num_original_ids] = self.original_id
 
     return self.graph
 
   def extract_fsa(self, node):
     "Constructs the segment of the fsa associated with a node in the forest"
+    
 
     # memoization if we have done this node already
     if self.memo.has_key(node.id):
@@ -191,51 +220,86 @@ class NodeExtractor(object):
     down_state = NonTermNode(self.graph, node, DOWN)
     up_state = NonTermNode(self.graph, node, UP) 
     self.memo[node.id] = (down_state, up_state)
-    
+  
+    link = {} 
     for edge in node.edge:
-      previous_state = down_state
-    
+      self.total_edges +=1 
       rhs = edge.tail_node_ids
       
-      # always start with the parent down state ( . P )       
-      nts_num = 0
+      phraselet = []
+      last = -2
+
+      edge_dot = {}
       for i,node_id in enumerate(rhs):
         to_node = self.forest.node[node_id]
-        
-        # next is a word ( . lex ) 
         if to_node.Extensions[is_word]:
-          new_state = LexNode(self.graph, to_node.Extensions[word].encode("UTF-8"), edge.id)
-
-          previous_state.add_edge(new_state)
-
-          # Move the dot ( lex . )
-          previous_state = new_state          
-
+          # it's a word, phraselet it and stick it in the list (later)  
+          phraselet.append(to_node)
         else:
-          # it's a symbol
+          # we've hit a new node, remember to make a link
+          key = (last, to_node.id, bool(phraselet))
+          link.setdefault(key, [])
+          link[key].append((edge, phraselet))
+          phraselet = []
+          last = to_node.id
+      key = (last, -1, bool(phraselet))
+      link.setdefault(key, [])
+      link[key].append((edge, phraselet))
+      phraselet = []
+      #print link
+    for ((from_node, to_node, my_has_phrases), my_phraselets) in link.iteritems():      
+      #(_, phraselet) = my_phraselets[0]
+
+      #words = " ".join([n.Extensions[word]for n in phraselet])
+      new_state = LexNode(self.graph, "", self.edge_id)
+      print "Phraselet len", len(my_phraselets)
+      new_state.proto.Extensions[has_phrases] = my_has_phrases 
+      for edge_dot, plet in my_phraselets:
+        proto_plet = new_state.proto.Extensions[phraselets].phraselet.add()
+        proto_plet.phraselet_hypergraph_edge = edge_dot.id
+        
+        for w in plet:  
+          subword = proto_plet.word.add()
+          subword.word = w.Extensions[word]
+          subword.subword_original_id = self.original_id
+          print self.original_id, subword.word
+          self.original_id += 1
 
 
-          # local symbol name (lagrangians!)
-          #pos = get_sym_pos(sym)
-          #to_node = edge.subs[nts_num]
-          #nts_num += 1
-          # We are at (. N_id ) need to get to ( N_id .) 
+      if from_node == -2:
+        previous_node = down_state
+        previous_node.add_edge(new_state, "")                
+      else:
+        _, previous_node = self.extract_fsa(self.forest.node[from_node])
+        protoedge = previous_node.add_edge(new_state, ( " UP").decode('UTF-8'))
+        #for edge_dot, phraselet in phraselets:
+        protoedge.label = "UP"
+        original = protoedge.Extensions[origin]
+        original.has_origin = True
+        original.original_id = self.original_id
+        self.original_id += 1
+        for edge_dot, _ in my_phraselets:
+          original.hypergraph_edge.append(edge_dot.id)
+      
 
-          # First, Create a unique named version of this state (. N_id) and ( N_id . )
-          # We need these so that we can assign lagrangians
-          local_down_state = InternalNode(self.graph, edge.label.encode("UTF-8"), i, to_node.id, DOWN, edge.id)
-          local_up_state = InternalNode(self.graph, edge.label.encode("UTF-8"), i , to_node.id, UP, edge.id)
+      if to_node == -1:
+        next_node = up_state
+        protoedge = new_state.add_edge(next_node, "")#(edge.label.encode('UTF-8') + " UP").decode('UTF-8') )
 
-          down_sym, up_sym = self.extract_fsa(to_node)
-          
-          previous_state.add_edge(local_down_state)
-          local_down_state.add_edge(down_sym)
-          up_sym.add_edge(local_up_state)
+      else:
+        next_node, _ = self.extract_fsa(self.forest.node[to_node])
+        protoedge = new_state.add_edge(next_node, ( " DOWN").decode('UTF-8') )
+        protoedge.label = "DOWN"
+        #for edge_dot, phraselet in phraselets:
+        original = protoedge.Extensions[origin]
+        original.has_origin = True
+        original.original_id = self.original_id
+        self.original_id += 1
+        for edge_dot, _ in my_phraselets:
+          original.hypergraph_edge.append(edge_dot.id)
 
-          # move the dot
-          previous_state = local_up_state
-          
-      previous_state.add_edge(up_state)
+
+      self.edge_id +=1 
     return self.memo[node.id]
 
 
@@ -252,3 +316,53 @@ if __name__ == "__main__":
   f = open(sys.argv[2], "wb")
   f.write(graph.lattice.SerializeToString())
   f.close()
+
+
+
+    # for edge in node.edge:
+    #   previous_state = down_state
+    
+    #   rhs = edge.tail_node_ids
+      
+    #   # always start with the parent down state ( . P )       
+    #   nts_num = 0
+
+
+    #   for i,node_id in enumerate(rhs):
+    #     to_node = self.forest.node[node_id]
+        
+    #     # next is a word ( . lex ) 
+    #     if to_node.Extensions[is_word]:
+    #       new_state = LexNode(self.graph, to_node.Extensions[word].encode("UTF-8"), edge.id)
+    #       self.word_nodes += 1
+    #       previous_state.add_edge(new_state)
+
+    #       # Move the dot ( lex . )
+    #       previous_state = new_state          
+
+    #     else:
+    #       # it's a symbol
+
+
+    #       # local symbol name (lagrangians!)
+    #       #pos = get_sym_pos(sym)
+    #       #to_node = edge.subs[nts_num]
+    #       #nts_num += 1
+    #       # We are at (. N_id ) need to get to ( N_id .) 
+
+    #       # First, Create a unique named version of this state (. N_id) and ( N_id . )
+    #       # We need these so that we can assign lagrangians
+    #       local_down_state = InternalNode(self.graph, edge.label.encode("UTF-8"), i, to_node.id, DOWN, edge.id)
+    #       local_up_state = InternalNode(self.graph, edge.label.encode("UTF-8"), i , to_node.id, UP, edge.id)
+    #       self.internal_nodes+=2
+
+    #       down_sym, up_sym = self.extract_fsa(to_node)
+          
+    #       previous_state.add_edge(local_down_state)
+    #       local_down_state.add_edge(down_sym)
+    #       up_sym.add_edge(local_up_state)
+
+    #       # move the dot
+    #       previous_state = local_up_state
+          
+    #   previous_state.add_edge(up_state)
--- util/reverse_lattice.py
+++ util/reverse_lattice.py
@@ -11,7 +11,7 @@ def reverse_lat(lat):
     by_id[n.id] = n 
     for e in n.edge:
       back_edges.setdefault(e.to_id, [])
-      back_edges[e.to_id].append((n.id, e.id))
+      back_edges[e.to_id].append((n.id, e))
   
   ret = Lattice()
   ret.start = lat.final[0]
@@ -21,19 +21,41 @@ def reverse_lat(lat):
     node.id = n_id
     node.label = by_id[n_id].label
     
-    node.Extensions[is_word] = by_id[n_id].Extensions[is_word]
-    node.Extensions[word] = by_id[n_id].Extensions[word]
-    print by_id[n_id].Extensions[word], n_id
+    
+    node.Extensions[has_phrases] = by_id[n_id].Extensions[has_phrases]
+    
+
+    for old_plet in by_id[n_id].Extensions[phraselets].phraselet:
+      plet = node.Extensions[phraselets].phraselet.add()
+      plet.phraselet_hypergraph_edge = old_plet.phraselet_hypergraph_edge
+      old = list(old_plet.word)
+      old.reverse()
+      for sword in old: 
+        new_word = plet.word.add()
+        new_word.subword_original_id = sword.subword_original_id
+        new_word.word = sword.word
+    #node.Extensions[is_word] = by_id[n_id].Extensions[is_word]
+    #node.Extensions[word] = by_id[n_id].Extensions[word]
+    #print by_id[n_id].Extensions[word], n_id
     
     
-    node.Extensions[original_node] = by_id[n_id].Extensions[original_node]
-    node.Extensions[ignore_node] = by_id[n_id].Extensions[ignore_node]
+    #node.Extensions[original_node] = by_id[n_id].Extensions[original_node]
+    #node.Extensions[ignore_node] = by_id[n_id].Extensions[ignore_node]
     
     if  n_id in back_edges:
-      for to_node,e_id in back_edges[n_id]:
+      for to_node,e in back_edges[n_id]:
         edge = node.edge.add()
         edge.to_id = to_node
-        edge.id =e_id
+        
+        edge.id =e.id
+        edge.label =e.label
+        
+        edge.Extensions[origin].original_id = e.Extensions[origin].original_id
+        for i in e.Extensions[origin].hypergraph_edge:
+          edge.Extensions[origin].hypergraph_edge.append(i)
+        edge.Extensions[origin].has_origin = e.Extensions[origin].has_origin
+  ret.Extensions[num_hypergraph_edges] = lat.Extensions[num_hypergraph_edges]
+  ret.Extensions[num_original_ids] = lat.Extensions[num_original_ids]
   return ret
 
 if __name__ == "__main__":
